e0dacbf49c6c4db3ae5dad4f4200c9c0c9836b3e
蒋鹏 <itdrjp@163.com>
2018-06-01 09:56:52 +0800

    增加排序练习

--- src/cn/practice/sort/FastSort.java
+++ src/cn/practice/sort/FastSort.java
@@ -10,16 +10,16 @@ package cn.practice.sort;
 public class FastSort {
     public static void quickSort(int[] list){
         if(list.length > 1){
-            select a pivot;
+            //select a pivot;
             //一般默认主元为第一个元素，同时从两边向中间比较
             //前向找到一个大于主元的元素，后向找到一个小于主元的元素，交换这两个元素
             //直到前向元素下标大于后向元素下标，把主元与此时后向元素交换，即主元前的元素
             //都小于主元，主元后的元素都大于主元。
-            partition list into list1 and list2 such that
-            all elements in list1 <= pivot and
-            all elements in list2 >pivot;
-            quickSort(list1);
-            quickSort(list2);
+            //partition list into list1 and list2 such that
+            //all elements in list1 <= pivot and
+            //all elements in list2 >pivot;
+            //quickSort(list1);
+            //quickSort(list2);
         }
     }
 }
